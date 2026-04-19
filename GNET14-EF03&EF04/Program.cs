263ee004d4c6561463ada2b8681549ed0fa02ab3
jasmine <jasminemohamed174005@gmail.com>
2026-04-19 13:09:17 +0200

    Add project files.

---
+++ GNET14-EF03&EF04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GNET14_EF03_EF04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
